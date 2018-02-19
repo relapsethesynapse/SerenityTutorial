735b366c3f7cb54232cc3009ba5b5785b2dbc2ce
suraut <suraut>
2018-02-19 10:39:23 +0530

    Committing the StudentApp project

---
+++ StudentApp/src/test/java/com/studentapp/junit/studentsinfo/CreateStudentsDataDrivenTest.java
@@ -0,0 +1,73 @@
+package com.studentapp.junit.studentsinfo;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.studentapp.cucumber.serenity.StudentSerenitySteps;
+import com.studentapp.testbase.TestBase;
+
+import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import net.thucydides.junit.annotations.UseTestDataFrom;
+
+@UseTestDataFrom("testdata/studentinfo.csv")
+@RunWith(SerenityParameterizedRunner.class)
+public class CreateStudentsDataDrivenTest extends TestBase{
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getProgramme() {
+		return programme;
+	}
+	public void setProgramme(String programme) {
+		this.programme = programme;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public StudentSerenitySteps getSteps() {
+		return steps;
+	}
+	public void setSteps(StudentSerenitySteps steps) {
+		this.steps = steps;
+	}
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String programme;
+	private String course;	
+	
+	@Steps
+	StudentSerenitySteps steps;
+	@Title("Data Driven Test for Adding Multiple Students to the StudentsApp")
+	@Test
+	public void createMultipleStudents() {
+		
+		ArrayList<String> courses = new ArrayList<>();
+		courses.add(course);
+		steps.createStudent(firstName, lastName, email, programme, courses).statusCode(201);
+	}
+}
---
+++ StudentApp/src/test/java/com/studentapp/testbase/TestBase.java
@@ -0,0 +1,13 @@
+package com.studentapp.testbase;
+
+import org.junit.BeforeClass;
+
+import io.restassured.RestAssured;
+
+public class TestBase {
+
+	@BeforeClass
+	public static void init() {
+		RestAssured.baseURI="http://ofss310399:8020/student";		
+	}
+}
